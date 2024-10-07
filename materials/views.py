c889494c9d418a4a35674e96d47bd5cb66ae50f8
yura_filchakov <filchakov1993@gmail.com>
2024-10-07 16:43:11 +0700

    views.py

--- materials/views.py
+++ materials/views.py
@@ -1,50 +1,74 @@
-from rest_framework.generics import (
-    CreateAPIView,
-    DestroyAPIView,
-    ListAPIView,
-    RetrieveAPIView,
-    UpdateAPIView,
-)
+from rest_framework.generics import (CreateAPIView, DestroyAPIView,
+                                     ListAPIView, RetrieveAPIView,
+                                     UpdateAPIView)
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 from materials.models import Course, Lesson
-from materials.serializer import (
-    CourseSerializer,
-    LessonSerializer,
-    CourseDetailSerializer,
-)
+from materials.serializer import (CourseDetailSerializer, CourseSerializer,
+                                  LessonSerializer)
+from users.permissions import IsModerator, IsOwner
 
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
-
+    
     def get_serializer_class(self):
         if self.action == "retrieve":
             return CourseDetailSerializer
         return CourseSerializer
+    
+    def perform_create(self, serializer):
+        course = serializer.save()
+        course.owner = self.request.user
+        course.save()
+    
+    def get_permissions(self):
+        if self.action == "create":
+            self.permission_classes = [~IsModerator]
+        elif self.action in ['retrieve', 'update']:
+            self.permission_classes = [IsAdminUser, IsModerator | IsOwner]
+        elif self.action in ['destroy']:
+            self.permission_classes = [~IsModerator | IsOwner]
+        return [permission() for permission in self.permission_classes]
 
 
 class LessonCreateApiView(CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated]
+    
+    def perform_create(self, serializer):
+        lesson = serializer.save()
+        lesson.owner = self.request.user
+        lesson.save()
+    
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            self.permission_classes = [IsAuthenticated]
+        return [permission() for permission in self.permission_classes]
 
 
 class LessonListApiView(ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, ~IsModerator | IsOwner]
 
 
 class LessonRetrieveApiView(RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, IsModerator | IsOwner]
 
 
 class LessonUpdateApiView(UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, IsModerator | IsOwner]
 
 
 class LessonDestroyApiView(DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = [IsAuthenticated, ~IsModerator | IsOwner]
--- users/views.py
+++ users/views.py
@@ -1,42 +1,35 @@
-from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
-from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
-from users.models import User, Payment
-from users.serializer import UserSerializer, PaymentSerializer
+from users.models import Payment, User
+from users.serializer import PaymentSerializer, UserSerializer
 
 
 class PaymentViewSet(ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
-    ordering_fields = ("payment_date", "payment_amount",)
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    ]
+    ordering_fields = (
+        "payment_date",
+        "payment_amount",
+    )
     search_fields = ("payment_method",)
     filterset_fields = ("payment_date", "paid_course", "paid_lesson", "payment_method")
-
-
-class UserListApiView(ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
 
 
 class UserCreateApiView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [AllowAny]
 
-
-class UserRetrieveApiView(RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserUpdateApiView(UpdateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    
-    
-class UserDestroyApiView(DestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
