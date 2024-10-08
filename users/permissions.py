525305160dff3a097c144fb84d60699e5b994f80
yura_filchakov <filchakov1993@gmail.com>
2024-10-08 19:08:57 +0700

    permissions.py

--- users/permissions.py
+++ users/permissions.py
@@ -3,12 +3,14 @@ from rest_framework import permissions
 
 class IsModerator(permissions.BasePermission):
     """Проверяет, является пользователь модератором."""
+
     def has_permission(self, request, view):
         return request.user.groups.filter(name="moderators").exists()
-    
-    
+
+
 class IsOwner(permissions.BasePermission):
     """Проверяет, является пользователь владельцем."""
+
     def has_object_permission(self, request, view, obj):
         if obj.owner == request.user:
             return True
