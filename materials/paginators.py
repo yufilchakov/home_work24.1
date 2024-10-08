c0105ea3649165123b9ab9629aad423a7963e78d
yura_filchakov <filchakov1993@gmail.com>
2024-10-08 19:10:24 +0700

    paginators.py

---
+++ materials/paginators.py
@@ -0,0 +1,17 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class LessonPagination(PageNumberPagination):
+    """Пагинация для списка уроков."""
+
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+
+class CoursePagination(PageNumberPagination):
+    """Пагинация для списка курсов."""
+
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 100
