2e24d8dab7d11b05c5a40eda96068d03b9816e33
yura_filchakov <filchakov1993@gmail.com>
2024-10-08 19:09:25 +0700

    serializer.py

--- materials/serializer.py
+++ materials/serializer.py
@@ -1,24 +1,40 @@
-from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from rest_framework import serializers
 
-from materials.models import Course, Lesson
+from materials.models import Course, Lesson, Subscription
+from materials.validators import validate_url
 
 
-class LessonSerializer(ModelSerializer):
+class LessonSerializer(serializers.ModelSerializer):
+    """Реализация сериализатора для уроков."""
+
+    validators = [validate_url]
+
     class Meta:
+        validators = [validate_url]
         model = Lesson
         fields = "__all__"
 
 
-class CourseSerializer(ModelSerializer):
+class CourseSerializer(serializers.ModelSerializer):
+    """Реализация сериализатора для курсов."""
+
     lessons = LessonSerializer(many=True, read_only=True, source="lesson_set")
+    validators = [validate_url]
+    is_subscribed = serializers.SerializerMethodField()
+
+    def get_is_subscribed(self, obj):
+        user = self.context["request"].user
+        return Subscription.objects.filter(user=user, course=obj).exists()
 
     class Meta:
         model = Course
         fields = "__all__"
 
 
-class CourseDetailSerializer(ModelSerializer):
-    number_lessons = SerializerMethodField()
+class CourseDetailSerializer(serializers.ModelSerializer):
+    """Реализация сериализатора для детальной информации о курсе."""
+
+    number_lessons = serializers.SerializerMethodField()
     lessons = LessonSerializer(many=True, read_only=True, source="lesson_set")
 
     def get_number_lessons(self, course):
@@ -27,3 +43,11 @@ class CourseDetailSerializer(ModelSerializer):
     class Meta:
         model = Course
         fields = ("name", "preview", "description", "lessons", "number_lessons")
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    """Реализация сериализатора для подписок."""
+
+    class Meta:
+        model = Subscription
+        fields = "__all__"
--- users/serializer.py
+++ users/serializer.py
@@ -4,12 +4,16 @@ from users.models import Payment, User
 
 
 class UserSerializer(ModelSerializer):
+    """Реализация сериализатора для пользователя."""
+
     class Meta:
         model = User
         fields = "__all__"
 
 
 class PaymentSerializer(ModelSerializer):
+    """Реализация сериализатора для платежей."""
+
     class Meta:
         model = Payment
         fields = "__all__"
