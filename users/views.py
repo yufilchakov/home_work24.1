d2f67bd164056fd667f28679bf30a89edf88a309
yura_filchakov <filchakov1993@gmail.com>
2024-10-10 01:04:48 +0700

    views.py

--- users/views.py
+++ users/views.py
@@ -1,16 +1,30 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from users.models import Payment, User
 from users.serializer import PaymentSerializer, UserSerializer
+from users.services import create_stripe_product, create_stripe_price, create_stripe_session
+
+
+class UserCreateApiView(CreateAPIView):
+    """Класс является представлением API для создания нового пользователя."""
+    
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+    
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
 
 
 class PaymentViewSet(ModelViewSet):
     """Класс является представлением API для управления платежами."""
-
+    
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
     filter_backends = [
@@ -26,14 +40,19 @@ class PaymentViewSet(ModelViewSet):
     filterset_fields = ("payment_date", "paid_course", "paid_lesson", "payment_method")
 
 
-class UserCreateApiView(CreateAPIView):
-    """Класс является представлением API для создания нового пользователя."""
-
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [AllowAny]
-
+class PaymentCreateAPIView(CreateAPIView):
+    """Класс является представлением API для создания нового платежа."""
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    permission_classes = [IsAuthenticated]
+    
     def perform_create(self, serializer):
-        user = serializer.save(is_active=True)
-        user.set_password(user.password)
-        user.save()
+        payment = serializer.save()
+        payment.user = self.request.user
+        stripe_product_id = create_stripe_product(payment)
+        payment.amount = payment.payment_amount
+        price = create_stripe_price(amount=payment.amount, stripe_product_id=stripe_product_id)
+        session_id, payment_link = create_stripe_session(price=price)
+        payment.session_id = session_id
+        payment.payment_url = payment_link
+        payment.save()
