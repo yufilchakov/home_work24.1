de66e978474acab4532ee43210fcd41455c909db
yura_filchakov <filchakov1993@gmail.com>
2024-10-10 01:04:29 +0700

    settings.py

--- config/settings.py
+++ config/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "rest_framework",
     "django_filters",
+    "drf_yasg",
     "users",
     "materials",
 ]
@@ -115,3 +116,5 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+
+STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
