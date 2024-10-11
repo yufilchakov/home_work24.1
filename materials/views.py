35d5402e3f44ed59f17d65ccfd95770d881bcdd8
yura_filchakov <filchakov1993@gmail.com>
2024-10-12 02:24:45 +0700

    views.py

--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from rest_framework.generics import (CreateAPIView, DestroyAPIView,
                                      ListAPIView, RetrieveAPIView,
                                      UpdateAPIView, get_object_or_404)
@@ -10,6 +12,7 @@ from materials.models import Course, Lesson, Subscription
 from materials.paginators import CoursePagination, LessonPagination
 from materials.serializer import (CourseDetailSerializer, CourseSerializer,
                                   LessonSerializer, SubscriptionSerializer)
+from materials.tasks import send_update_notification
 from users.permissions import IsModerator, IsOwner
 
 
@@ -38,6 +41,16 @@ class CourseViewSet(ModelViewSet):
         elif self.action in ["destroy"]:
             self.permission_classes = [~IsModerator | IsOwner]
         return [permission() for permission in self.permission_classes]
+
+    def form_valid(self, form):
+        course = form.save()
+        users = course.subscribers.all()
+        if (
+            not course.updated_at
+            or (datetime.now() - course.updated_at).total_seconds() / 3600 > 4
+        ):
+            send_update_notification.delay(course, users)
+        return super().form_valid(form)
 
 
 class LessonCreateApiView(CreateAPIView):
@@ -81,6 +94,17 @@ class LessonUpdateApiView(UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
     permission_classes = [IsAuthenticated, IsModerator | IsOwner]
+
+    def form_valid(self, form):
+        lesson = form.save()
+        course = lesson.course
+        users = course.subscribers.all()
+        if (
+            not lesson.updated_at
+            or (datetime.now() - lesson.updated_at).total_seconds() / 3600 > 4
+        ):
+            send_update_notification.delay(course, users)
+        return super().form_valid(form)
 
 
 class LessonDestroyApiView(DestroyAPIView):
--- users/views.py
+++ users/views.py
@@ -6,16 +6,17 @@ from rest_framework.viewsets import ModelViewSet
 
 from users.models import Payment, User
 from users.serializer import PaymentSerializer, UserSerializer
-from users.services import create_stripe_product, create_stripe_price, create_stripe_session
+from users.services import (create_stripe_price, create_stripe_product,
+                            create_stripe_session)
 
 
 class UserCreateApiView(CreateAPIView):
     """Класс является представлением API для создания нового пользователя."""
-    
+
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
-    
+
     def perform_create(self, serializer):
         user = serializer.save(is_active=True)
         user.set_password(user.password)
@@ -24,7 +25,7 @@ class UserCreateApiView(CreateAPIView):
 
 class PaymentViewSet(ModelViewSet):
     """Класс является представлением API для управления платежами."""
-    
+
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
     filter_backends = [
@@ -42,16 +43,19 @@ class PaymentViewSet(ModelViewSet):
 
 class PaymentCreateAPIView(CreateAPIView):
     """Класс является представлением API для создания нового платежа."""
+
     serializer_class = PaymentSerializer
     queryset = Payment.objects.all()
     permission_classes = [IsAuthenticated]
-    
+
     def perform_create(self, serializer):
         payment = serializer.save()
         payment.user = self.request.user
         stripe_product_id = create_stripe_product(payment)
         payment.amount = payment.payment_amount
-        price = create_stripe_price(amount=payment.amount, stripe_product_id=stripe_product_id)
+        price = create_stripe_price(
+            amount=payment.amount, stripe_product_id=stripe_product_id
+        )
         session_id, payment_link = create_stripe_session(price=price)
         payment.session_id = session_id
         payment.payment_url = payment_link
